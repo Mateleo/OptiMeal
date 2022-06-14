70247dfdab74637ce5f1444fc1b2a0bfe2a5db3f
Mateleo <mateo.dominguez@efrei.net>
2022-06-14 10:44:41 +0200

    setup tailwind css

--- site/src/main.ts
+++ site/src/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+//@ts-ignore
 import App from './App.vue'
 import router from './router'
+import './index.css'
 
 const app = createApp(App)
 
